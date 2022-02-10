260692e41124587db500e0e5a0b013375a529475
kelogub <belogub.konstantin200202@yandex.ru>
2022-02-10 23:27:04 +0800

    initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+    var items = document.getElementsByClassName("item__content--hidden")
+    for (let item of items) {
+        item.parentElement.addEventListener("click", function(){
+            if (this.getElementsByClassName("item__content")[0].classList.contains("item__content--hidden")){
+                this.getElementsByClassName("item__content")[0].classList.remove("item__content--hidden")
+            } else {
+                this.getElementsByClassName("item__content")[0].classList.add("item__content--hidden")
+            }
+        })
+    }
+}
